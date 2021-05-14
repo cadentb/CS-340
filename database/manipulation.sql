8778ba3579d4db7e187ba52040f5b802fe205973
mbrayton27 <mbrayton27@gmail.com>
2021-05-13 22:12:34 -0700

    again

--- database/manipulation.sql
+++ database/manipulation.sql
@@ -2,10 +2,10 @@ INSERT INTO customers(firstName, lastName, email)
 VALUES (:firstNameValue, :lastNameValue, :emailValue)
 
 INSERT INTO orders(shipStreet, shipCity, shipState, shipZip)
-VALUES (shipStreetValue, shipCityValue, shipStateValue, shipZipValue)
+VALUES (:shipStreetValue, :shipCityValue, :shipStateValue, :shipZipValue)
 
 INSERT INTO products(category, price, name, quantityInStock)
-VALUES (categoryValue, priceValue, nameValue, quantityInStockValue)
+VALUES (:categoryValue, :priceValue, :nameValue, :quantityInStockValue)
 
 INSERT INTO greekHouses(letters, nickname)
-VALUES (lettersValue, nicknameValue)
+VALUES (:lettersValue, :nicknameValue)
